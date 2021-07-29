f62f3b204376154a0ca82d8e15c449f66a0d9965
adamalcantara <alcantaradevelopment@gmail.com>
2021-07-29 14:53:39 -0400

    Page files added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,14 +3,14 @@ import React, { Component } from 'react'
 class Navbar extends Component {
     state = { clicked: false }
 
-        handleClick = () => {
-            this.setState({clicked: !this.state.clicked})
-        }
-    
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+
     render() {
         return (
             <div>
-                
+
             </div>
         )
     }
