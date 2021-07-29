c9babe6268ddbefeb579d9303b7ec17111d8a275
adamalcantara <alcantaradevelopment@gmail.com>
2021-07-29 14:51:59 -0400

    handleclick function added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 
 class Navbar extends Component {
+    state = { clicked: false }
+
+        handleClick = () => {
+            this.setState({clicked: !this.state.clicked})
+        }
+    
     render() {
         return (
             <div>
